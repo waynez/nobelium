f0b97a9db1c52b037626da407d78b37eb18c66f2
waynez <wayne.zhengw@gmail.com>
2023-04-11 20:58:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'WayneZ',
+  author: 'Wayne Z.',
+  email: 'i@waynez.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
